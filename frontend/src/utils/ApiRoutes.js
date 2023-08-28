58e7550ccf8a70f01c92740317881cfcde5601ad
Utsav Agrawal <agrutsav88728@gmail.com>
2023-08-28 18:31:55 +0530

    changes

--- frontend/src/utils/ApiRoutes.js
+++ frontend/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host="https://chat-app-gl15.onrender.com"
+export const host="https://chat-app-ebon-three.vercel.app"
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute=`${host}/api/auth/setAvatar`;
